43daf29c6ee42b38c53f36a34d1785346581041d
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-25 23:30:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function saveLoginDetails() {
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+      var rememberMe = document.getElementById("checkbox").checked;
+
+      if (rememberMe) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+      }
+
+      alert("Logged in as " + username);
+}
+
+function loginAsExistingUser() {
+      var savedUsername = localStorage.getItem("username");
+      alert("Logged in as " + savedUsername);
+}
